71093309a49868721287dfd52b21ae1ef9eb9a87
SamuelJordan101 <44780790+SamuelJordan101@users.noreply.github.com>
2020-01-09 18:11:23 +0000

    Fixed links
    links were broken on  socem, so applied file extensions to fix

--- Code/PubApplication/public/admin.php
+++ Code/PubApplication/public/admin.php
@@ -7,10 +7,10 @@ include_once 'header.php';
 
 <body style="text-align: center">
     <br><h1>Admin Options</h1><br>
-    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminitemnew';">Add New Items</button>
-    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminitemedit';">Edit Items</button>
-    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminitemview';">View Items</button><br><br>
-    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminview';">View Orders</button>
+    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminitemnew.php';">Add New Items</button>
+    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminitemedit.php';">Edit Items</button>
+    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminitemview.php';">View Items</button><br><br>
+    <button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'adminview.php';">View Orders</button>
 </body>
 
 <?php
--- Code/PubApplication/public/index.php
+++ Code/PubApplication/public/index.php
@@ -8,10 +8,10 @@ include_once '../src/model/item.php';
 <body style="text-align: center" style="min-height: 80%">
 <br>
 <h1>User Options</h1><br>
-<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'ordernew';">Add New Order</button>
-<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'orderadd';">Add To Order</button>
-<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'ordercancel';">Cancel Orders</button><br><br>
-<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'orderview';">View Orders</button>
+<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'ordernew.php';">Add New Order</button>
+<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'orderadd.php';">Add To Order</button>
+<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'ordercancel.php';">Cancel Orders</button><br><br>
+<button type="button" class="w-25 btn btn-secondary" onclick="window.location.href = 'orderview.php';">View Orders</button>
 </body>
 
 <?php
